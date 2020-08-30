3c45644d0a762358b463bf5d5cfe04c14c24676f
Unknown <krypt-lynx@yandex.ru>
2020-08-30 09:40:03 +0200

    pivoting objective fix

--- Cassowary/ClSimplexSolver.cs
+++ Cassowary/ClSimplexSolver.cs
@@ -371,7 +371,7 @@ namespace Cassowary
                     }
                 }
 
-                if (exitVar != null)
+                if (exitVar != null && exitVar != _objective)
                 {
                     Pivot(marker, exitVar);
                 }

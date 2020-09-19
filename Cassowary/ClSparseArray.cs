ad156fdf9d6ab72998ab59769912990e2c7af426
Unknown <krypt-lynx@yandex.ru>
2020-09-19 09:49:14 +0200

    fix

--- Cassowary/ClSparseArray.cs
+++ Cassowary/ClSparseArray.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Cassowary
+namespace Cassowary_moddiff
 {
     public abstract partial class ClAbstractVariable
     {
